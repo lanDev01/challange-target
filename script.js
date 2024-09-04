973f8a013728180e77af7fcd1830cb2d786226ef
LanDev01 <alnjr0001@outlook.com>
2024-09-04 10:02:54 -0300

    Resolvendo quarto desafio

--- script.js
+++ script.js
@@ -79,3 +79,68 @@ console.log("Menor faturamento do mês: R$", menorFaturamento);
 console.log("Maior faturamento do mês: R$", maiorFaturamento);
 console.log("Número de dias com faturamento acima da média mensal:", diasAcimaDaMedia);
 
+// 4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+// • SP – R$67.836,43
+// • RJ – R$36.678,66
+// • MG – R$29.229,88
+// • ES – R$27.165,48
+// • Outros – R$19.849,53
+
+// Dados de faturamento por estado
+const faturamentoEstados = {
+  SP: 67836.43,
+  RJ: 36678.66,
+  MG: 29229.88,
+  ES: 27165.48,
+  Outros: 19849.53
+};
+
+// Dados de faturamento diário
+const faturamentoDiarioTotal = [
+  { dia: 1, valor: 22174.1664 },
+  { dia: 2, valor: 24537.6698 },
+  { dia: 3, valor: 26139.6134 },
+  { dia: 4, valor: 0.0 },
+  { dia: 5, valor: 0.0 },
+  { dia: 6, valor: 26742.6612 },
+  { dia: 7, valor: 0.0 },
+  { dia: 8, valor: 42889.2258 },
+  { dia: 9, valor: 46251.174 },
+  { dia: 10, valor: 11191.4722 },
+  { dia: 11, valor: 0.0 },
+  { dia: 12, valor: 0.0 },
+  { dia: 13, valor: 3847.4823 },
+  { dia: 14, valor: 373.7838 },
+  { dia: 15, valor: 2659.7563 },
+  { dia: 16, valor: 48924.2448 },
+  { dia: 17, valor: 18419.2614 },
+  { dia: 18, valor: 0.0 },
+  { dia: 19, valor: 0.0 },
+  { dia: 20, valor: 35240.1826 },
+  { dia: 21, valor: 43829.1667 },
+  { dia: 22, valor: 18235.6852 },
+  { dia: 23, valor: 4355.0662 },
+  { dia: 24, valor: 13327.1025 },
+  { dia: 25, valor: 0.0 },
+  { dia: 26, valor: 0.0 },
+  { dia: 27, valor: 25681.8318 },
+  { dia: 28, valor: 1718.1221 },
+  { dia: 29, valor: 13220.495 },
+  { dia: 30, valor: 8414.61 }
+];
+
+// Calcula o faturamento total mensal
+const faturamentoTotal = faturamentoDiarioTotal.reduce((acc, item) => acc + item.valor, 0);
+
+// Calcula a porcentagem de cada estado em relação ao faturamento total
+const porcentagensEstados = {};
+for (const estado in faturamentoEstados) {
+  porcentagensEstados[estado] = ((faturamentoEstados[estado] / faturamentoTotal) * 100).toFixed(2) + '%';
+}
+
+// Exibe o faturamento total e a porcentagem de cada estado
+console.log("Faturamento Total Mensal: R$", faturamentoTotal.toFixed(2));
+for (const estado in porcentagensEstados) {
+  console.log(`Porcentagem do faturamento de ${estado}: ${porcentagensEstados[estado]}`);
+}
+
