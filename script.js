b3821fc2f4f54518c601159ff8c8cbeaab7113f2
LanDev01 <alnjr0001@outlook.com>
2024-09-04 09:27:16 -0300

    Resolvendo segundo desafio

--- script.js
+++ script.js
@@ -16,4 +16,38 @@ while (K < INDICE) {
   SOMA = SOMA + K;
 }
 
-console.log(SOMA);
+console.log(SOMA); // Resultado é 91
+
+////////////////
+
+// 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor 
+// sempre será a soma dos 2 valores anteriores(exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), 
+// escreva um programa na linguagem que desejar onde, informado um número, 
+// ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado
+// pertence ou não a sequência.
+
+function isFibonacci(num) {
+  if (num === 0 || num === 1) {
+    return `O número ${num} pertence à sequência de Fibonacci.`;
+  }
+
+  let a = 0, b = 1;
+  let fib = a + b;
+
+  while (fib <= num) {
+    if (fib === num) {
+      return `O número ${num} pertence à sequência de Fibonacci.`;
+    }
+    a = b;
+    b = fib;
+    fib = a + b;
+  }
+
+  return `O número ${num} não pertence à sequência de Fibonacci.`;
+}
+
+const resultado = isFibonacci(21); // pertence a sequência de Fibonacci
+// const resultado = isFibonacci(96); // não pertence a sequência de Fibonacci
+console.log(resultado);
+
+//////////
